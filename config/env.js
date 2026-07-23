9896e73b780b75d6dd56759ad493a8b5ae2c62c0
pepeeteless <pepeeteless@hotmail.com>
2026-07-22 23:05:47 -0600

    feat: add `compose.yaml` and config `zod` to parse `process.env`

---
+++ config/env.js
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.coerce.number().default(5432),
+  POSTGRES_USER: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error(
+    "❌ Existe erros nas variaveis de ambiente: ",
+    _env.error.format(),
+  );
+  process.exit(1);
+}
+
+export const env = _env.data;
---
+++ infra/database.js
@@ -0,0 +1,25 @@
+import { Client } from "pg";
+import { env } from "../config/env";
+
+async function query(queryObject) {
+  const client = new Client({
+    user: env.POSTGRES_USER,
+    host: env.POSTGRES_HOST,
+    port: env.POSTGRES_PORT,
+    database: env.POSTGRES_DB,
+    password: env.POSTGRES_PASSWORD,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    if (client) {
+      await client.end();
+    }
+  }
+}
